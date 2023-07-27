0a08023122330d200bae2d9d7ff8539ec7fb2fa0
Omomayowa Adebanjo <omayowfade@gmail.com>
2023-07-27 15:57:07 -0400

    Organize application into relevant groups + remove boilerplate app/scene delegate comments  + code .

---
+++ URL Shortener Application/Support/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  URL Shortener Application
+//
+//  Created by Omomayowa Adebanjo on 7/27/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
